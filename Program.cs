150586ed01c15b86cb4444ff874ca84dbf6be044
Евгений Кучма <desferto87@gmail.com>
2025-02-22 11:52:44 +0500

    lv

--- Program.cs
+++ Program.cs
@@ -4,29 +4,29 @@ namespace KansasCityShuffle
 {
     internal class Program
     {
-
         static void Shaffle(int[] numbers)
         {
-            Random random = new Random();
-            int numberRandom;
+            int randomNumbers;
             int temp;
 
-            for (int i = numbers.Length - 1; i > 0; i--)
+            Random random = new Random();
+
+            for(int i = numbers.Length - 1; i > 0; i--)
             {
-                numberRandom = random.Next(i + 1);
-                temp = numbers[numberRandom];
-                numbers[numberRandom] = numbers[i];
-                numbers[i] = temp;
+                randomNumbers = random.Next(i + 1);
+                temp = numbers[i];
+                numbers[i] = numbers[randomNumbers];
+                numbers[randomNumbers] = temp;
             }
-           
-            for(int i = 0; i < numbers.Length; i++)
+
+            for(int i = 0;  i < numbers.Length; i++)
             {
-                Console.WriteLine(numbers[i]);
+                Console.Write(numbers[i] + " ");
             }
         }
         static void Main(string[] args)
         {
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             Shaffle(numbers);
         }
