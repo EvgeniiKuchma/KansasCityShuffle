0763bcdef20275c9ea970f138a20cedeb07d9f51
Евгений Кучма <desferto87@gmail.com>
2025-02-22 11:37:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KansasCityShuffle
+{
+    internal class Program
+    {
+
+        static void Shaffle(int[] numbers)
+        {
+            Random random = new Random();
+            int numberRandom;
+            int temp;
+
+            for (int i = numbers.Length - 1; i > 0; i--)
+            {
+                numberRandom = random.Next(i + 1);
+                temp = numbers[numberRandom];
+                numbers[numberRandom] = numbers[i];
+                numbers[i] = temp;
+            }
+           
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Shaffle(numbers);
+        }
+    }
+}
